1b6c22cb2731b468ee5c84e04545a4cc39baac8a
giov27 <giovannimaendra@gmail.com>
2022-07-30 23:50:32 +0700

    [FIxed] Update after review

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,38 +1,43 @@
-const { headerTitles, facilities, classes, instructors, mediaPartners, testimonies } = require('./jsonData')
+const {
+  headerTitles,
+  facilities,
+  classes,
+  instructors,
+  mediaPartners,
+  testimonies,
+} = require('./jsonData');
 
-exports.getHomepageView = (req, res) =>{
-    // all variable
+exports.getHomepageView = (req, res) => {
+  // all variable
 
-    // render
-    return res.render('pages/index', {
-        facilites: facilities,
-        classes: classes,
-        instructors: instructors,
-        mediaPartners: mediaPartners,
-        testimonies: testimonies
-    })
-}
+  // render
+  return res.render('pages/index', {
+    facilites: facilities,
+    classes: classes,
+    instructors: instructors,
+    mediaPartners: mediaPartners,
+    testimonies: testimonies,
+  });
+};
 
-exports.getInstructorView = (req, res) =>{
+exports.getInstructorView = (req, res) => {
+  return res.render('pages/instructor', {
+    headerTitle: headerTitles.instructor,
+    instructors: instructors,
+  });
+};
 
-    return res.render('pages/instructor', {
-        headerTitle: headerTitles.instructor,
-        instructors: instructors
-    })
-}
+exports.getAboutView = (req, res) => {
+  return res.render('pages/about', {
+    headerTitle: headerTitles.about,
+    facilites: facilities,
+    mediaPartners: mediaPartners,
+    API_KEY: '',
+  });
+};
 
-exports.getAboutView = (req, res) =>{
-
-    return res.render('pages/about', {
-        headerTitle: headerTitles.about,
-        facilites: facilities,
-        mediaPartners: mediaPartners
-    })
-}
-
-exports.getMembershipView = (req, res) =>{
-
-    return res.render('pages/membership', {
-        headerTitle: headerTitles.membership
-    })
-}
+exports.getMembershipView = (req, res) => {
+  return res.render('pages/membership', {
+    headerTitle: headerTitles.membership,
+  });
+};
--- controllers/jsonData.js
+++ controllers/jsonData.js
@@ -1,229 +1,237 @@
 module.exports = {
   headerTitles: {
-    instructor: "Instruktur Sanggar Segar",
-    membership: "Membership Sanggar Segar",
-    about: "Tentang Sanggar Segar",
+    instructor: 'Instruktur Studio Segar',
+    membership: 'Membership Studio Segar',
+    about: 'Tentang Studio Segar',
   },
   facilities: [
     {
-      img: "/img/facility/fasilitasKelas.png",
-      altText: "Fasilitas Studio Sanggar",
-      description: "Studio Sanggar",
+      img: '/img/facility/fasilitasKelas.png',
+      altText: 'Fasilitas Studio Sanggar',
+      description: 'Studio Sanggar',
     },
     {
-      img: "/img/facility/fasilitasBarbel.png",
-      altText: "Fasilitas Barbel",
-      description: "Barbel",
+      img: '/img/facility/fasilitasBarbel.png',
+      altText: 'Fasilitas Barbel',
+      description: 'Barbel',
     },
     {
-      img: "/img/facility/fasilitasMatras.png",
-      altText: "Fasilitas Matras",
-      description: "Matras",
+      img: '/img/facility/fasilitasMatras.png',
+      altText: 'Fasilitas Matras',
+      description: 'Matras',
     },
   ],
-  instructors:[
+  instructors: [
     {
-      img: "/img/instructor/instruktur_praptiningsih.svg",
-      trainerName: "Dra. Praptiningsih",
-      trainerTitle: "Penguji UJK LSKSI",
-      trainerExp: "(Pengalaman 30 Tahun)",
-      trainerExpDesc: "Dra. Praptiningsih memiliki pengalaman sebagai instruktur pelatihan selama <b>30 tahun</b>, yang tersebar di,",
+      img: '/img/instructor/instruktur_praptiningsih.svg',
+      trainerName: 'Dra. Praptiningsih',
+      trainerTitle: 'Penguji UJK LSKSI',
+      trainerExp: '(Pengalaman 30 Tahun)',
+      trainerExpDesc:
+        'Dra. Praptiningsih memiliki pengalaman sebagai instruktur pelatihan selama <b>30 tahun</b>, yang tersebar di,',
       trainerCourses: [
-        "Marlupi Ballet", "Sanggar Senam Kartika Dewi", "Sanggar Senam Hanna",
-        "Sanggar Senam Noni Kusuma", "Merapi View", "LKP Segar"
-      ]
+        'Marlupi Ballet',
+        'Sanggar Senam Kartika Dewi',
+        'Sanggar Senam Hanna',
+        'Sanggar Senam Noni Kusuma',
+        'Merapi View',
+        'LKP Segar',
+      ],
     },
     {
-      img: "/img/instructor/instruktur_apri.png",
-      trainerName: "Apri Astuti",
-      trainerTitle: "Senam Level III",
-      trainerExp: "(Pengalaman 15 Tahun)",
-      trainerExpDesc: "Apri Astuti memiliki pengalaman sebagai instruktur pelatihan selama <b>15 tahun</b> yang tersebar di,",
+      img: '/img/instructor/instruktur_apri.png',
+      trainerName: 'Apri Astuti',
+      trainerTitle: 'Senam Level III',
+      trainerExp: '(Pengalaman 15 Tahun)',
+      trainerExpDesc:
+        'Apri Astuti memiliki pengalaman sebagai instruktur pelatihan selama <b>15 tahun</b> yang tersebar di,',
       trainerCourses: [
-        "BPR Bantul", "Sanggar Senam Puspita", "LKP Segar"
-      ]
+        'BPR Bantul',
+        'Sanggar Senam Puspita',
+        'LKP Segar',
+      ],
     },
     {
-      img: "/img/instructor/instruktur_atik.svg",
-      trainerName: "Apri Astuti",
-      trainerTitle: "Senam Level III",
-      trainerExp: "(Pengalaman 15 Tahun)",
-      trainerExpDesc: "Atik Maryani memiliki pengalaman sebagai instruktur pelatihan selama <b>15 tahun</b> yang tersebar di,",
-      trainerCourses: [
-      ]
-    }
+      img: '/img/instructor/instruktur_atik.svg',
+      trainerName: 'Apri Astuti',
+      trainerTitle: 'Senam Level III',
+      trainerExp: '(Pengalaman 15 Tahun)',
+      trainerExpDesc:
+        'Atik Maryani memiliki pengalaman sebagai instruktur pelatihan selama <b>15 tahun</b> yang tersebar di,',
+      trainerCourses: [],
+    },
   ],
   classes: {
     general: [
       {
-        img: "/img/kelas_senam1.png",
-        className: "Aerobic(Fun)",
+        img: '/img/kelas_senam1.png',
+        className: 'Aerobic Fun',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Aerobic(Body Language)",
+        img: '/img/kelas_senam1.png',
+        className: 'Aerobic Body Language',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Yoga dan Pilates",
+        img: '/img/kelas_senam1.png',
+        className: 'Yoga dan Pilates',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Line Dance",
+        img: '/img/kelas_senam1.png',
+        className: 'Line Dance',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Belly Dance",
+        img: '/img/kelas_senam1.png',
+        className: 'Belly Dance',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
     ],
     level: [
       {
-        img: "/img/kelas_senam1.png",
-        className: "Program Level II (Reguler)",
+        img: '/img/kelas_senam1.png',
+        className: 'Program Level II (Reguler)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Program Level III (Reguler)",
+        img: '/img/kelas_senam1.png',
+        className: 'Program Level III (Reguler)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Program Level II (Kelas Privat)",
+        img: '/img/kelas_senam1.png',
+        className: 'Program Level II (Kelas Privat)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Program Level III (Kelas Privat)",
+        img: '/img/kelas_senam1.png',
+        className: 'Program Level III (Kelas Privat)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
     ],
     banPer: [
       {
-        img: "/img/kelas_senam1.png",
-        className: "Kelas Bantuan Pemerintah",
+        img: '/img/kelas_senam1.png',
+        className: 'Kelas Bantuan Pemerintah',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
     ],
     special: [
       {
-        img: "/img/kelas_senam1.png",
-        className: "Line Dance (Kelas Instruktur)",
+        img: '/img/kelas_senam1.png',
+        className: 'Line Dance (Kelas Instruktur)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Belly Dance (Kelas Instruktur)",
+        img: '/img/kelas_senam1.png',
+        className: 'Belly Dance (Kelas Instruktur)',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
     ],
     instructor: [
       {
-        img: "/img/kelas_senam1.png",
-        className: "Body Language Exercise",
+        img: '/img/kelas_senam1.png',
+        className: 'Body Language Exercise',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Belly Dance",
+        img: '/img/kelas_senam1.png',
+        className: 'Belly Dance',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Jab and Kick",
+        img: '/img/kelas_senam1.png',
+        className: 'Jab & Kick',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Line Dance",
+        img: '/img/kelas_senam1.png',
+        className: 'Line Dance',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Step Dance",
+        img: '/img/kelas_senam1.png',
+        className: 'Step Dance',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
       {
-        img: "/img/kelas_senam1.png",
-        className: "Yoga dan Pilates",
+        img: '/img/kelas_senam1.png',
+        className: 'Yoga dan Pilates',
         description:
-          "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”",
+          '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
       },
     ],
   },
-  mediaPartners:[
+  mediaPartners: [
     {
-      img: "/img/mediaPartner/Partner_FA_Segar.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_FA_Segar.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_Bumiku_Handayani.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_Bumiku_Handayani.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_Sekar_Buana.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_Sekar_Buana.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_SS_Gayatri.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_SS_Gayatri.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_SS_Primaraga.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_SS_Primaraga.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_SS_Prisaba.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_SS_Prisaba.svg',
+      alt: '',
     },
     {
-      img: "/img/mediaPartner/Partner_SS_Puspita.svg",
-      alt: ""
+      img: '/img/mediaPartner/Partner_SS_Puspita.svg',
+      alt: '',
     },
   ],
   testimonies: [
     {
-      img: "/img/profile-testimoni.svg",
-      alt: "Bapak-bapak",
-      testimonyName: "Zildjan Raka",
-      testimonyJob: "Tukang Sundul",
-      desc: "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”"
+      img: '/img/profile-testimoni.svg',
+      alt: 'Bapak-bapak',
+      testimonyName: 'Zildjan Raka',
+      testimonyJob: 'Tukang Sundul',
+      desc: '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
     },
     {
-      img: "/img/profile-testimoni.svg",
-      alt: "Bapak-bapak",
-      testimonyName: "Dwiki Ganteng",
-      testimonyJob: "Tukang Batagor",
-      desc: "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”"
+      img: '/img/profile-testimoni.svg',
+      alt: 'Bapak-bapak',
+      testimonyName: 'Dwiki Ganteng',
+      testimonyJob: 'Tukang Batagor',
+      desc: '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
     },
     {
-      img: "/img/profile-testimoni.svg",
-      alt: "Bapak-bapak",
-      testimonyName: "Bur cakeup",
-      testimonyJob: "Tukang Cuci Sepatu",
-      desc: "“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”"
-    }
-  ]
+      img: '/img/profile-testimoni.svg',
+      alt: 'Bapak-bapak',
+      testimonyName: 'Bur cakeup',
+      testimonyJob: 'Tukang Cuci Sepatu',
+      desc: '“Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, lorem ipsum dolor sit amet.”',
+    },
+  ],
 };
